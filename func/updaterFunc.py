d5a675a79a24415ae26f637e1e3709ae99dc6512
Jonathan <jonathqncjr@gmail.com>
2025-10-26 15:45:38 +0700

    removed unused functions in updaterFunc.py

--- func/updaterFunc.py
+++ func/updaterFunc.py
@@ -1,17 +1,11 @@
-import os, sys, zipfile, shutil, requests, json
-from PyQt5.QtWidgets import QMessageBox
+import sys, requests, json
 from requests.auth import HTTPBasicAuth
 from packaging import version
 from pathlib import Path
-from tqdm import tqdm
 from PyQt5.QtCore import QThread, pyqtSignal
 
 # Konfigurasi
-REMOTE_JSON_URL = "https://raw.githubusercontent.com/AtlasCJr/ControlCenter/main/latest.json"
 LOCAL_VERSION_FILE = "app-config.json"
-UPDATE_ZIP_NAME = "update.zip"
-EXTRACT_DIR = "update_temp"
-APP_DIR = "."
 REPO = "AtlasCJr/ControlCenter"
 GITHUB_USER = "AtlasCJr"
 GITHUB_TOKEN = "***REMOVED***"
@@ -104,64 +98,3 @@ def list_assets():
     rel = r.json()
     assets = rel.get("assets", [])
     return rel.get("tag_name"), assets
-
-def download_zip(asset):
-    api_url = asset["url"]
-    headers = {
-        "Accept": "application/octet-stream",
-        "Authorization": f"token {GITHUB_TOKEN}",
-    }
-
-    with requests.get(api_url, headers=headers, stream=True) as r:
-        r.raise_for_status()
-        filename = asset["name"]
-
-        # Dapatkan total ukuran file dari header (dalam byte)
-        total_size = int(r.headers.get("Content-Length", 0))
-        block_size = 8192
-
-        print(f"📦 Downloading update: {filename}")
-
-        # tqdm untuk progress bar
-        with open(filename, "wb") as f, tqdm(
-            total=total_size,
-            unit="B",
-            unit_scale=True,
-            unit_divisor=1024,
-            desc="🔽 Progress",
-            ncols=80,
-        ) as progress:
-            for chunk in r.iter_content(chunk_size=block_size):
-                if chunk:
-                    f.write(chunk)
-                    progress.update(len(chunk))
-
-        print(f"✅ Download finished: {filename}")
-
-
-def extract_and_replace():
-    print("📦 Mengekstrak...")
-    with zipfile.ZipFile(UPDATE_ZIP_NAME, "r") as zip_ref:
-        zip_ref.extractall(EXTRACT_DIR)
-
-    print("🧹 Menimpa file lama...")
-    for name in os.listdir(EXTRACT_DIR):
-        src = os.path.join(EXTRACT_DIR, name)
-        dst = os.path.join(APP_DIR, name)
-
-        if os.path.exists(dst):
-            if os.path.isdir(dst):
-                shutil.rmtree(dst)
-            else:
-                os.remove(dst)
-
-        shutil.move(src, dst)
-
-    os.remove(UPDATE_ZIP_NAME)
-    shutil.rmtree(EXTRACT_DIR)
-
-
-def restart_app():
-    print("🔁 Restarting...")
-    python = sys.executable
-    os.execl(python, python, *sys.argv)
