82964b644dc308869b37f3aabcfe4fd8542f12ff
eDarrrr <haidaralghifari112@gmail.com>
2026-02-27 15:17:01 +0700

    fix some issue so that now the installer.sh can automaticly install in the lab computer

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@
 PROJECT_DIR=$(pwd)
 DESKTOP_FILE="$HOME/.local/share/applications/control-center.desktop"
 cd "$PROJECT_DIR"
+pip install pyinstaller
+sudo dnf install python3.11 python3.11-devel
 pyinstaller --noconsole --onedir --name ControlCenter pages/Home/main.py
+
 
 # Bikin file .desktop dengan path yang menyesuaikan laptop masing-masing
 cat <<EOF > "$DESKTOP_FILE"
